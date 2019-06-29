423f2a77ed796654830c3bb8364991375b062100
unknown <chugm1989@gmail.com>
2019-06-29 17:52:01 -0400

    Updates

---
+++ precache-manifest.4907090e8756691c2a7357fefd9be93e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9115005d121d4b15ee8bab1622ff9b6a",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "a5e566f327d03d70bd46",
+    "url": "/clicky-game/static/css/main.62f1d4e3.chunk.css"
+  },
+  {
+    "revision": "00063f64c68494124901",
+    "url": "/clicky-game/static/js/2.49afd6f5.chunk.js"
+  },
+  {
+    "revision": "a5e566f327d03d70bd46",
+    "url": "/clicky-game/static/js/main.88469c36.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
